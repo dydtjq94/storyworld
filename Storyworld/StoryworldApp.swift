30fb0fe450a8ae3f8b20d38658f0733ed5bb9b8d
dydtjq94 <ghdtlr90@gmail.com>
2025-01-08 18:23:30 +0900

    Initial Commit

---
+++ Storyworld/StoryworldApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoryworldApp.swift
+//  Storyworld
+//
+//  Created by peter on 1/8/25.
+//
+
+import SwiftUI
+
+@main
+struct StoryworldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StoryworldTests/StoryworldTests.swift
@@ -0,0 +1,17 @@
+//
+//  StoryworldTests.swift
+//  StoryworldTests
+//
+//  Created by peter on 1/8/25.
+//
+
+import Testing
+@testable import Storyworld
+
+struct StoryworldTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
